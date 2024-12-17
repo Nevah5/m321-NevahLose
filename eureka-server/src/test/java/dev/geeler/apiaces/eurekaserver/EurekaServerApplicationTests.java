015ce8846a2d49a6da2b0e2516b4529c4d2bbb57
Nevah5 <contact@nevah5.dev>
2024-12-17 16:35:45 +0100

    Added `eureka-server` backend

---
+++ eureka-server/src/test/java/dev/geeler/apiaces/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.geeler.apiaces.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
