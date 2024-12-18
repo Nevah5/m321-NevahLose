019dd637260ba649df76c3785e5aaaedef831905
Nevah5 <contact@nevah5.dev>
2024-12-18 16:35:07 +0100

    Added join game functionality into the game service

--- backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/service/GameServiceImpl.java
+++ backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/service/GameServiceImpl.java
@@ -1,6 +1,8 @@
 package dev.geeler.apiaces.gameservice.service;
 
 import dev.geeler.apiaces.gameservice.model.Game;
+import dev.geeler.apiaces.gameservice.model.GamePlayer;
+import dev.geeler.apiaces.gameservice.repository.GamePlayerRepository;
 import dev.geeler.apiaces.gameservice.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.UUID;
 public class GameServiceImpl implements GameService {
     @Autowired
     private GameRepository gameRepository;
+    @Autowired
+    private GamePlayerRepository gamePlayerRepository;
 
     @Override
     public Game createGame(UUID playerId) {
@@ -23,11 +27,33 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public Game getGame(UUID gameId) {
-        return null;
+        return gameRepository.findById(gameId).orElse(null);
     }
 
     @Override
     public Game joinGame(String roomId, UUID playerId) {
-        return null;
+        // TODO: improve this mess
+        final Game game = gameRepository.findByRoomId(roomId);
+        if (game == null) {
+            return null;
+        }
+        if (game.getOwnerId().equals(playerId)) {
+            return game;
+        }
+        GamePlayer gamePlayer = gamePlayerRepository.findByPlayerIdAndGameId(playerId, game.getId());
+        if (gamePlayer != null) {
+            return null;
+        }
+        if (gamePlayerRepository.findByGameId(game.getId()).size() >= game.getMaxPlayers()) {
+            return null;
+        }
+        gamePlayer = new GamePlayer.Builder()
+                .setGameId(game.getId())
+                .setPlayerId(playerId)
+                .build();
+
+        gamePlayerRepository.save(gamePlayer);
+        gameRepository.save(game);
+        return game;
     }
 }
