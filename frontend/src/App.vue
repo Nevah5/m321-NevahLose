a803f0df18cb519af93e438037d7f773eed4d017
Nevah5 <contact@nevah5.dev>
2024-12-19 10:44:37 +0100

    Added loading overlay

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,12 @@
 import { RouterView } from "vue-router";
 import HeaderComponent from "./components/HeaderComponent.vue";
 import FooterComponent from "./components/FooterComponent.vue";
+import LoadingOverlay from "./components/LoadingOverlay.vue";
 </script>
 
 <template>
   <div>
+    <LoadingOverlay />
     <HeaderComponent />
     <RouterView />
     <FooterComponent />
--- frontend/src/components/icons/LogoIcon.vue
+++ frontend/src/components/icons/LogoIcon.vue
@@ -1,9 +1,25 @@
 <template>
-  <router-link to="/" class="logo">
-    <img alt="API Aces Logo" class="logo" src="@/assets/logo.svg" />
+  <router-link to="/" class="logo" v-if="!disableLink">
+    <img
+      alt="API Aces Logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      :width="logoWidth"
+    />
   </router-link>
+  <img
+    v-else
+    alt="API Aces Logo"
+    class="logo"
+    src="@/assets/logo.svg"
+    :width="logoWidth"
+  />
 </template>
 
 <script lang="ts" setup>
 import { RouterLink } from "vue-router";
+const { logoWidth = 120, disableLink = false } = defineProps<{
+  logoWidth?: number;
+  disableLink?: boolean;
+}>();
 </script>
