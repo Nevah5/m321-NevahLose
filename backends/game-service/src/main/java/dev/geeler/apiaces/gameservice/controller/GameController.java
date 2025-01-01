4cd141cc8d0f2bb871b47f86372bd5aeb5ba7a78
Nevah5 <contact@nevah5.dev>
2025-01-01 23:38:26 +0100

    Added basic websocket endpoint

--- backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/controller/GameController.java
+++ backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/controller/GameController.java
@@ -4,19 +4,22 @@ import dev.geeler.apiaces.gameservice.dto.CreateGameDto;
 import dev.geeler.apiaces.gameservice.model.Game;
 import dev.geeler.apiaces.gameservice.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RestController
-@RequestMapping("/games")
+@Controller
 public class GameController {
     @Autowired
     private GameService gameService;
 
     // TODO: implement the endpoints
 
-    @PostMapping()
+    @MessageMapping("/join")
+    @SendTo("/topic/game")
     public Game createGame(@RequestBody CreateGameDto createGameDto) {
         return gameService.createGame(UUID.fromString(createGameDto.getPlayerId()));
     }
