e1626e0fce0d983f065e182dfdccc89999e78bca
Nevah5 <contact@nevah5.dev>
2025-01-05 00:16:45 +0100

    Added WebSocketConfig#configureMessageConverters

--- backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/config/WebSocketConfig.java
+++ backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/config/WebSocketConfig.java
@@ -1,10 +1,18 @@
 package dev.geeler.apiaces.gameservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +27,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setContentTypeResolver(resolver);
+        converter.setObjectMapper(new ObjectMapper());
+        messageConverters.add(converter);
+        
+        return false;
     }
 }
