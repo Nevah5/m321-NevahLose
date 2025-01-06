8ba2ad1e067725e785434e859db93a1d4c73ac87
Nevah5 <contact@nevah5.dev>
2025-01-06 23:07:21 +0100

    Added plane icon

---
+++ frontend/src/components/icons/PlaneIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <img
+    src="@/assets/plane.svg"
+    alt="Paper plane icon"
+    :width="iconWidth"
+    :class="color"
+  />
+</template>
+
+<script setup lang="ts">
+const { iconWidth = 128, color = "" } = defineProps<{
+  iconWidth?: number;
+  color?: "white";
+}>();
+</script>
+
+<style scoped lang="scss">
+img.white {
+  filter: invert(100%);
+}
+</style>
