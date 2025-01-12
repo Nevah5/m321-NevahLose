fd8335136470fcf7a192bdbd9e86727790cdc552
Nevah5 <contact@nevah5.dev>
2025-01-12 17:42:56 +0100

    Added card service interface #2

---
+++ backends/game-service/src/main/java/dev/geeler/apiaces/gameservice/service/CardService.java
@@ -0,0 +1,9 @@
+package dev.geeler.apiaces.gameservice.service;
+
+import dev.geeler.apiaces.gameservice.model.card.Card;
+
+import java.util.List;
+
+public interface CardService {
+    List<Card> getCards();
+}
